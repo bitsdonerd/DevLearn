72102b01d480a4a8a658891b620485d73b604e43
bitsdonerd <lukasousa70@gmail.com>
2024-12-15 11:42:52 -0300

    add scripts `lint:check` and `lint:fix `

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 // Primeira função/componente do react
-function Home(){
-    return <h1>Hello world</h1>
+function Home() {
+  return <h1>Hello world</h1>;
 }
+
+function testeFormato() {
+  console.log('Formato errado bora ver ao salvar')
+}
+
 
 export default Home;
