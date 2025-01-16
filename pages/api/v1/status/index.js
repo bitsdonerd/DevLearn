4ff1ba93179823fa46a239ceeb4de2f3f6a99756
bitsdonerd <lukasousa70@gmail.com>
2025-01-16 12:27:25 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,41 @@
 import database from "root/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Response OK" });
+  // Data - toISOString
+  const updatedAt = new Date().toISOString();
+
+  // Postgres version
+  const result = await database.query("SHOW server_version;");
+  const postgresVersion = result.rows[0].server_version;
+  console.log("Postgres version:", postgresVersion);
+
+  // Postgres max conections
+  const connection = await database.query("SHOW max_connections;");
+  const postgresConnection = connection.rows[0].max_connections;
+  console.log("Max connections:", postgresConnection);
+
+  // Postgres open connections
+  const databaseName = process.env.POSTGRES_DB;
+  // "SELECT count(*) FROM pg_stat_activity;" - to get the number of open connections
+  const usedConnection = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpenedConnections = usedConnection.rows[0].count;
+  console.log("Opened connections:", databaseOpenedConnections);
+
+  // API Rest - updated_at (snake_case)
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: parseInt(postgresConnection),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+
+  });
 }
 
 export default status;
