7687bd7f24990ed5a0e8cc3cc8487551b5325c90
bitsdonerd <lukasousa70@gmail.com>
2025-02-17 14:35:40 -0300

    added new folder structure

--- infra/database.js
+++ infra/database.js
@@ -10,7 +10,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true, // SSL should be enabled in production
+    ssl: getSSLValues(),
   });
 
   try {
@@ -27,6 +27,9 @@ async function query(queryObject) {
 
 }
 
+function getSSLValues() {
+  return process.env.NODE_ENV === "development" ? false : true; // SSL should be enabled in production
+}
 
 export default {
   query: query
