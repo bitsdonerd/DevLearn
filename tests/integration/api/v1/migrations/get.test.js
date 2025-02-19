7beeacda95ec468900a9e0c044a1f69304bba557
bitsdonerd <lukasousa70@gmail.com>
2025-02-19 18:34:46 -0300

    add migration test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,16 @@
+import database from "infra/database";
+
+
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   console.log(responseBody);
+
   // Array of migrations
   expect(Array.isArray(responseBody)).toEqual(true);
-
+  // Check if the first migration has content
+  expect(responseBody.length).toBeGreaterThan(0);
 });
