0a5d993dadefefaa9f4fcd91309191248eb1a096
bitsdonerd <lukasousa70@gmail.com>
2025-03-14 14:51:45 -0300

    adds `/api/v1/migrations ` endpoint

--- infra/database.js
+++ infra/database.js
@@ -4,17 +4,9 @@ import { Client } from "pg";
 
 async function query(queryObject) {
 
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
 
@@ -27,10 +19,33 @@ async function query(queryObject) {
 
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+
+  });
+
+  await client.connect();
+  return client;
+}
+
 function getSSLValues() {
-  return process.env.NODE_ENV === "development" ? false : true; // SSL should be enabled in production
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    }
+  }
+
+
+  return process.env.NODE_ENV === "production" ? true : false; // SSL should be enabled in production
 }
 
 export default {
-  query: query
+  query,
+  getNewClient
 }
