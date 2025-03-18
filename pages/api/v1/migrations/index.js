1b8f35f0a14f1f2dd4a214bad158eefc7cd127e7
bitsdonerd <lukasousa70@gmail.com>
2025-03-18 19:44:01 -0300

    fix bug with dbClient

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,40 +3,47 @@ import { join } from "node:path"
 import database from 'infra/database';
 
 export default async function migrations(request, response) {
-
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  }
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations)
+  try {
+    const dbClient = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
     }
 
-    return response.status(200).json(migratedMigrations);
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
+
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+
+      await dbClient.end();
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations)
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await dbClient.end();
+    return response.status(405).end();
   }
 
-  return response.status(405).end();
+
+
 
 
 }
