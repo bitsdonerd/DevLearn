64732d3d863f5b04b83d4336855fae0a93ae6240
bitsdonerd <lukasousa70@gmail.com>
2025-04-22 20:02:58 -0300

    add a class for loading at `wait-for-postgres.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -17,3 +17,25 @@ function checkPostgres() {
 process.stdout.write('\n\n 🔴 Waiting for PostgreSQL connections to be ready \n\n');
 checkPostgres()
 
+
+class LoadingSpinner {
+  constructor(message = "Waiting for PostgreSQL connections to be ready", intervalTime = 100) {
+    this.message = message;
+    this.intervalTime = intervalTime;
+    this.loadingIndicator = 0;
+    this.loadingFrames = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+    this.interval = null;
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      const frame = this.loadingFrames[this.loadingIndicator++ % this.loadingFrames.length];
+      process.stdout.write(`\r\x1b[33m${frame} ${this.message}\x1b[0m `);
+    }, this.intervalTime);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    process.stdout.write("\r\x1b[32m✔ PostgreSQL is ready!\x1b[0m\n");
+  }
+}
