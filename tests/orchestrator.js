a165518b29d309d4748b97cb1986c3e00adbc7fb
bitsdonerd <lukasousa70@gmail.com>
2025-05-29 14:17:38 -0300

    add more performance using maxTimeout as a limit for time retries

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,11 +6,19 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     })
 
     async function fetchStatusPage() {
-      const response = await fetch('http://localhost:3000/api/v1/status');
-      const responseBopdy = await response.json();
+      try {
+        const response = await fetch('http://localhost:3000/api/v1/status');
+        if (response.status !== 200) {
+          throw Error(`HTTP error: ${response.status}`);
+        }
+      } catch (error) {
+        throw error;
+      }
+
     }
   }
 }
