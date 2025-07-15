32fbf9269eb8d130410ddf1af9d7109ceafeb460
bitsdonerd <lukasousa70@gmail.com>
2025-07-15 20:36:26 -0300

    fixed vercel failed build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 // Primeira função/componente do react
 function Home() {
-  return <h1>Página em construção! Aguarde por novidades...</h1>;
+  return (
+    <h1>
+      Página em construção! Aguarde por novidades...
+    </h1>
+  )
 }
 
 export default Home;
