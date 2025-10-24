8e210ad83cfd08c5c36e9fee453d88133743f36e
Lucas Martins <lukasousa70@gmail.com>
2025-10-24 20:33:04 -0300

    fix: aplica o lint no codigo atual

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -9,14 +9,14 @@ router.post(postMigrationsHandler);
 export default router.handler(controller.errorHandlers);
 
 async function postMigrationsHandler(request, response) {
-  // Importante entender como os dados de uma request entram aqui no controller 
+  // Importante entender como os dados de uma request entram aqui no controller
   // -- como quando alguem realizar um post contra esse endpoint (username, email, password)
   const userInputValues = request.body;
 
-  // Aqui deve ser a logica de criacao do usuario dentro do model 
+  // Aqui deve ser a logica de criacao do usuario dentro do model
   const newUser = await user.create(userInputValues);
 
-  // O model deve retornar o usuario criado no Banco de Dados e 
-  // tambem retornar o registro aqui no controller para permitir a resposta no endpoint 
-  return response.status(201).json(newUser); // Retorna o registro do usuario criado no endpoint 
+  // O model deve retornar o usuario criado no Banco de Dados e
+  // tambem retornar o registro aqui no controller para permitir a resposta no endpoint
+  return response.status(201).json(newUser); // Retorna o registro do usuario criado no endpoint
 }
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -10,7 +10,6 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
         // Especifique que o corpo da requisição(tipo dos dados) é JSON
@@ -22,7 +21,7 @@ describe("POST /api/v1/users", () => {
           username: "bitsdonerd",
           email: "bitsdonerd@gmail.com",
           password: "senha123",
-        })
+        }),
       });
 
       // Verifica os usuarios no banco apos a criacao
@@ -32,17 +31,16 @@ describe("POST /api/v1/users", () => {
 
       expect(responseBody).toEqual({
         id: responseBody.id,
-        username: 'bitsdonerd',
-        email: 'bitsdonerd@gmail.com',
-        password: 'senha123',
+        username: "bitsdonerd",
+        email: "bitsdonerd@gmail.com",
+        password: "senha123",
         created_at: responseBody.created_at,
-        updated_at: responseBody.updated_at, // tunelamento da resposta para ser ela mesma 
+        updated_at: responseBody.updated_at, // tunelamento da resposta para ser ela mesma
       });
 
       expect(uuidVersion(responseBody.id)).toBe(4); // Verifica se o ID é um UUID v4
       expect(Date.parse(responseBody.created_at)).not.toBeNaN(); // Verifica se created_at é uma data válida
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
-
     });
   });
 });
