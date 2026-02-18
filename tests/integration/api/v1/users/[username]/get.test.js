fa8f3ef026522c50f8cb07276378bacaaf9e95c6
Lucas Martins <lukasousa70@gmail.com>
2026-02-18 18:36:25 -0300

    feat: create and user `orchestrator.createUser()`

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -10,19 +10,11 @@ beforeAll(async () => {
 describe("GET /api/v1/users/[username]", () => {
   describe("Anonymous user", () => {
     test("With exact case match", async () => {
-      const response1 = await fetch("http://localhost:3000/api/v1/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: "CamelCaseUser",
-          email: "snake_case@curso.dev",
-          password: "senha123",
-        }),
+      await orchestrator.createUser({
+        username: "CamelCaseUser",
+        email: "snake_case@curso.dev",
+        password: "senha123",
       });
-
-      expect(response1.status).toBe(201);
 
       const response2 = await fetch(
         "http://localhost:3000/api/v1/users/CamelCaseUser",
@@ -47,19 +39,11 @@ describe("GET /api/v1/users/[username]", () => {
     });
 
     test("With case missmatch", async () => {
-      const response1 = await fetch("http://localhost:3000/api/v1/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: "DifferentCaseUser",
-          email: "different_case@curso.dev",
-          password: "senha123",
-        }),
+      await orchestrator.createUser({
+        username: "DifferentCaseUser",
+        email: "different_case@curso.dev",
+        password: "senha123",
       });
-
-      expect(response1.status).toBe(201);
 
       const response2 = await fetch(
         "http://localhost:3000/api/v1/users/differentcaseuser",
